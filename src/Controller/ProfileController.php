3bb9f583324436c8b3522061416f191ed06d17e4
Harry Youd <harry@harryyoud.co.uk>
2022-11-10 21:53:01 +0000

    Add profile management

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\ProfileType;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[IsGranted('ROLE_USER')]
+class ProfileController extends AbstractController {
+    public function __construct(
+        private readonly EntityManagerInterface $em,
+        private readonly UserPasswordHasherInterface $passwordHasher,
+    ) {}
+
+    #[Route('/profile', name: 'profile')]
+    public function profile(Request $request): Response {
+        $user = $this->getUser();
+        $form = $this->createForm(ProfileType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var User $user */
+            $user = $form->getData();
+            if (!is_null($form->get('password')->getData()) && !empty($form->get('password')->getData())) {
+                $hashedPassword = $this->passwordHasher->hashPassword(
+                    $user,
+                    $form->get('password')->getData(),
+                );
+                $user->setPassword($hashedPassword);
+            }
+            $this->em->persist($user);
+            $this->em->flush();
+            $this->addFlash("success", "User changes submitted");
+            return $this->redirectToRoute('profile');
+        }
+        return $this->renderForm('profile.html.twig', [
+            'form' => $form
+        ]);
+
+    }
+}
---
+++ src/Form/ProfileType.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Doctrine\DBAL\Schema\UniqueConstraint;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+use Symfony\Component\Validator\Constraints\Email;
+
+class ProfileType extends AbstractType {
+    public function __construct(
+    ) {}
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
+        $builder
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'autocomplete' => 'name',
+                ]
+            ])
+            ->add('email', TextType::class, [
+                'attr' => [
+                    'autocomplete' => 'email',
+                ],
+                'constraints' => [
+                    new Email(),
+                ]
+            ])
+            ->add('currentPassword', PasswordType::class, [
+                'mapped' => false,
+                'required' => true,
+                'help' => 'Type your current password to save changes',
+                'constraints' => [
+                    new UserPassword(),
+                ],
+                'attr' => [
+                    'autocomplete' => 'current-password',
+                ],
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'required' => false,
+                'first_options' => [
+                    'label' => 'Password',
+                    'help' => 'Leave blank to leave unchanged',
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                    ],
+                ],
+                'second_options' => [
+                    'label' => 'Repeat password',
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                    ],
+                ],
+            ])
+            ->add('save', SubmitType::class)
+        ;
+    }
+}
