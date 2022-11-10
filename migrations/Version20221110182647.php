87536b93acc433e08d6b6ed14d3a3cbd33d25813
Harry Youd <harry@harryyoud.co.uk>
2022-11-10 18:31:56 +0000

    Add user entity

---
+++ migrations/Version20221110182647.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20221110182647 extends AbstractMigration {
+    public function getDescription(): string {
+        return 'Add users';
+    }
+
+    public function up(Schema $schema): void {
+        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles CLOB NOT NULL --(DC2Type:json)
+        , password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
+    }
+
+    public function down(Schema $schema): void {
+        $this->addSql('DROP TABLE user');
+    }
+}
