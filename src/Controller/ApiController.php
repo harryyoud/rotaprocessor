4d0606f5fb69e371ebe2d28d7b147d62fcf65fbe
Harry Youd <harry@harryyoud.co.uk>
2022-11-13 18:59:42 +0000

    Secure job log endpoint

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\SyncJob;
+use App\Security\SyncJobVoter;
 use JsonException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,6 +19,7 @@ class ApiController extends AbstractController {
     public function __construct() {}
 
     #[Route('/job/{id}/log', name: 'get_job_log_json')]
+    #[IsGranted(SyncJobVoter::VIEW_LOGS, subject: 'job')]
     public function getJobLog(SyncJob $job): JsonResponse {
         $text = $job->getLog();
         try {
