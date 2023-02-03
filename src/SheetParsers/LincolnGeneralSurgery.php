5d963cf7839a8918bbe5196386d316ba9f9d7eba
Harry Youd <harry@harryyoud.co.uk>
2023-02-03 00:06:53 +0000

    Fix datetime arguments

--- src/SheetParsers/LincolnGeneralSurgery.php
+++ src/SheetParsers/LincolnGeneralSurgery.php
@@ -11,9 +11,9 @@ class LincolnGeneralSurgery extends PilgrimGeneralSurgery {
 
     protected function getShiftTimes(string $cellValue, \DateTime $startDate): array {
         return match ($cellValue) {
-            'SEAU' => ['SEAU', new DateTimeImmutable($startDate->format('Y-m-d'), '08:00:00'), new DateInterval('PT12H30M')],
-            'Nights' => ['Nights', new DateTimeImmutable($startDate->format('Y-m-d'), '20:00:00'), new DateInterval('PT12H30M')],
-            'Cover' => ['Cover', new DateTimeImmutable($startDate->format('Y-m-d'), '08:00:00'), new DateInterval('PT12H30M')],
+            'SEAU' => ['SEAU', new DateTimeImmutable($startDate->format('Y-m-d') . '08:00:00'), new DateInterval('PT12H30M')],
+            'Nights' => ['Nights', new DateTimeImmutable($startDate->format('Y-m-d') . '20:00:00'), new DateInterval('PT12H30M')],
+            'Cover' => ['Cover', new DateTimeImmutable($startDate->format('Y-m-d') . '08:00:00'), new DateInterval('PT12H30M')],
             default => parent::getShiftTimes($cellValue, $startDate),
         };
     }
