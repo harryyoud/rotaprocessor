3e36b1b1da24186483287e607ffc35af1b540d52
Harry Youd <harry@harryyoud.co.uk>
2023-02-21 18:12:35 +0000

    Examples for placement form fields

--- src/Form/PlacementType.php
+++ src/Form/PlacementType.php
@@ -15,8 +15,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 class PlacementType extends AbstractType {
     public function __construct(
         private readonly SheetParsers $parsers
-    ) {
-    }
+    ) {}
 
     public function buildForm(FormBuilderInterface $builder, array $options): void {
         $builder
@@ -33,17 +32,17 @@ class PlacementType extends AbstractType {
                 'required' => false,
             ])
             ->add('calendarCategory', TextType::class, [
-                'help' => "Category for calendar events",
+                'help' => "Category for calendar events. Only necessary if attaching to WebDAV calendar",
             ])
             ->add('prefix', TextType::class, [
-                'help' => "Prefix to add to calendar events",
+                'help' => "Prefix to add to calendar events (e.g. \"Work - \")",
                 'trim' => false,
             ])
             ->add('nameFilter', TextType::class, [
-                'help' => "Exact contents of the row/column containing your shifts",
+                'help' => "Exact contents of the row/column header containing your shifts (e.g. \"J SMITH (FY2)\")",
             ])
             ->add('sheetName', TextType::class, [
-                'help' => "Name of the sheet of the Excel file",
+                'help' => "Name of the sheet of the Excel file (e.g. \"Junior Rota\")",
             ])
             ->add('save', SubmitType::class);
     }
