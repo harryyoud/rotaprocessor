4174d44a85c0ac6e93cdffaebfe2f252af8797a3
Harry Youd <harry@harryyoud.co.uk>
2023-03-03 16:36:18 +0000

    Update shift times and split SDT half-day for gensurg

--- src/SheetParsers/PilgrimGeneralSurgery.php
+++ src/SheetParsers/PilgrimGeneralSurgery.php
@@ -24,15 +24,17 @@ class PilgrimGeneralSurgery extends AbstractSheetParser {
         $shifts = [];
 
         foreach ($combinedShiftCells as [$dateCell, $shiftCell]) {
-            /** @var DateTimeImmutable $shiftStart */
-            /** @var DateInterval $shiftLength */
-            [$shiftName, $shiftStart, $shiftLength] = $this->getShiftTimes($shiftCell->getValue(), Date::excelToDateTimeObject($dateCell->getValue()));
-            if ($shiftName === "Off") {
-                continue;
+            $shiftsToAdd = $this->getShiftTimes($shiftCell->getValue(), Date::excelToDateTimeObject($dateCell->getValue()));
+            foreach ($shiftsToAdd as [$shiftName, $shiftStart, $shiftLength]) {
+                /** @var DateTimeImmutable $shiftStart */
+                /** @var DateInterval $shiftLength */
+                if ($shiftName === "Off") {
+                    continue;
+                }
+                $shiftEnd = $shiftStart->add($shiftLength);
+                $shift = new Shift($shiftName, $shiftStart, $shiftEnd);
+                $shifts[] = $shift;
             }
-            $shiftEnd = $shiftStart->add($shiftLength);
-            $shift = new Shift($shiftName, $shiftStart, $shiftEnd);
-            $shifts[] = $shift;
         }
 
         return $shifts;
@@ -40,22 +42,23 @@ class PilgrimGeneralSurgery extends AbstractSheetParser {
 
     protected function getShiftTimes(string $cellValue, \DateTime $startDate): array {
         if ($cellValue === "Cover") {
-            if (intval($startDate->format('w')) === 5) {
-                return ['Vascular Cover', new DateTimeImmutable($startDate->format('Y-m-d') . '11:00:00'), new DateInterval('PT9H')];
-            }
-            if (intval($startDate->format('w')) === 6 || intval($startDate->format('w')) === 0) {
-                return ['Vascular Cover', new DateTimeImmutable($startDate->format('Y-m-d') . '08:00:00'), new DateInterval('PT12H')];
-            }
-            return ['Vascular Cover', new DateTimeImmutable($startDate->format('Y-m-d') . '11:00:00'), new DateInterval('PT12H')];
+            return [[
+                'Vascular Cover',
+                new DateTimeImmutable($startDate->format('Y-m-d') . in_array(intval($startDate->format('w')), [0, 6]) ? '08:00:00' : '11:00:00'),
+                new DateInterval(intval($startDate->format('w')) === 5 ? 'PT9H' : 'PT12H'),
+            ]];
         }
         return match ($cellValue) {
-            'WARD' => ['Ward', new DateTimeImmutable($startDate->format('Y-m-d') . '08:00:00'), new DateInterval('PT8H')],
-            'OFF' => ['Off', null, null],
-            'SL' => ['Study Leave', new DateTimeImmutable($startDate->format('Y-m-d') . '08:00:00'), new DateInterval('PT8H')],
-            'AL' => ['Annual Leave', new DateTimeImmutable($startDate->format('Y-m-d') . '08:00:00'), new DateInterval('PT8H')],
-            'WRD/SDT' => ['SDT (Ward half-day)', new DateTimeImmutable($startDate->format('Y-m-d') . '08:00:00'), new DateInterval('PT4H')],
-            'DAYS' => ['Days', new DateTimeImmutable($startDate->format('Y-m-d') . '08:00:00'), new DateInterval('PT12H')],
-            'DAYS 8-4' => ['Day Helper', new DateTimeImmutable($startDate->format('Y-m-d') . '08:00:00'), new DateInterval('PT8H')],
+            'OFF' => [['Off', null, null]],
+            'WARD' => [['Ward', new DateTimeImmutable($startDate->format('Y-m-d') . '08:00:00'), new DateInterval('PT8H30M')]],
+            'SL' => [['Study Leave', new DateTimeImmutable($startDate->format('Y-m-d') . '08:00:00'), new DateInterval('PT8H30M')]],
+            'AL' => [['Annual Leave', new DateTimeImmutable($startDate->format('Y-m-d') . '08:00:00'), new DateInterval('PT8H30M')]],
+            'WRD/SDT' => [
+                ['Ward', new DateTimeImmutable($startDate->format('Y-m-d') . '08:00:00'), new DateInterval('PT4H')],
+                ['SDT', new DateTimeImmutable($startDate->format('Y-m-d') . '12:00:00'), new DateInterval('PT4H30M')],
+            ],
+            'DAYS' => [['Days', new DateTimeImmutable($startDate->format('Y-m-d') . '08:00:00'), new DateInterval('PT12H30M')]],
+            'DAYS 8-4' => [['Day Helper', new DateTimeImmutable($startDate->format('Y-m-d') . '08:00:00'), new DateInterval('PT8H30M')]],
         };
     }
 }
