b2e0f77ddcdbc32d42c0bcefc1ae49efdbdeedc5
Harry Youd <harry@youd.family>
2023-05-07 01:13:53 +0100

    Add weird shift at LCH surg

--- src/SheetParsers/LincolnGeneralSurgery.php
+++ src/SheetParsers/LincolnGeneralSurgery.php
@@ -12,6 +12,7 @@ class LincolnGeneralSurgery extends PilgrimGeneralSurgery {
     protected function getShiftTimes(string $cellValue, \DateTime $startDate): array {
         return match ($cellValue) {
             'SEAU' => [['SEAU', new DateTimeImmutable($startDate->format('Y-m-d') . '08:00:00'), new DateInterval('PT12H30M')]],
+            'SEAU-17.00' => [['SEAU (day time only)', new DateTimeImmutable($startDate->format('Y-m-d') . '08:00:00'), new DateTimeInterval('PT9H')]],
             'Nights' => [['Nights', new DateTimeImmutable($startDate->format('Y-m-d') . '20:00:00'), new DateInterval('PT12H30M')]],
             'Cover' => [['Cover', new DateTimeImmutable($startDate->format('Y-m-d') . '08:00:00'), new DateInterval('PT12H30M')]],
             default => parent::getShiftTimes($cellValue, $startDate),
