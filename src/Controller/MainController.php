edf53eafa2a2ae84c35dc7ab8a194443734de742
Harry Youd <harry@youd.family>
2026-02-07 02:00:28 +0000

    rector 2

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -21,6 +21,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Uid\Uuid;
 
